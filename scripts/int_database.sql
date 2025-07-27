cf39ba9d8a3eaa4b039cbe0f3df1ace5c1499e95
Roberto <chiaieseroberto@gmail.com>
2025-07-27 20:57:41 +0200

    Update int_database.sql

--- scripts/int_database.sql
+++ scripts/int_database.sql
@@ -1,1 +1,38 @@
+/*
+==========================================================
+CREATE DATABASE AND SCHEMAS
+==========================================================
+Scritp Purpose:
+  The script creates the database called 'DataWarehouse', in case the database already exists it will be dropped
+  in order to be replaced by a new one.
+  Afterwards, the three schemas are generated: bronze, silver and gold.
+*/
 
+
+USE master;
+
+IF EXISTS ( SELECT 1 FROM sys.databases WHERE name = 'DataWarehouse')
+BEGIN
+	ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+	DROP DATABASE DataWarehouse;
+END;
+GO
+
+
+--Create & Use the Database 
+CREATE DATABASE  DataWarehouse;
+GO
+
+USE DataWarehouse;
+GO
+
+
+--Create Three Schemas( Bronze, Silver, Gold)
+CREATE SCHEMA bronze;
+GO
+
+CREATE SCHEMA silver;
+GO
+
+CREATE SCHEMA gold;
+GO
